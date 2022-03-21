f653df0c3b64a1969f564125c1c3d9ad2e69c34b
Magali Fabri <magali_90@live.nl>
2022-03-21 15:53:46 +0100

    Linkify author mention on article show view

--- View/articles/show.php
+++ View/articles/show.php
@@ -7,7 +7,11 @@
     <h1><?= ucwords($article->title) ?></h1>
     <p><?= $article->formatPublishDate() ?></p>
     <p><?= $article->description ?></p>
-    <p>Written by <?= $article->authorName ?></p>
+    <p>Written by
+        <a href="?page=author&author-id=<?= $article->authorId ?>">
+            <?= $article->authorName ?>
+        </a>
+    </p>
 
     <?php if ($previous) : ?>
         <a href="?page=articles-details&article-id=<?= $previous ?>"">Previous article</a>
